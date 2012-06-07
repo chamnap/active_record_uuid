5296b0d322a99e849b2ae5a6745a628ef5f0505d
Chamnap <chamnapchhorn@gmail.com>
2012-06-07 22:27:05 +0700

    add rake tasks to fix db/schema.rb

--- lib/active_record_uuid.rb
+++ lib/active_record_uuid.rb
@@ -1,9 +1,9 @@
 require 'uuidtools'
 require 'active_record'
-require 'active_record_uuid/version'
 
 require 'active_record_uuid/uuid_base'
+require 'active_record_uuid/railtie'
 
 module ActiveRecordUuid
-  
+  autoload :VERSION, 'active_record_uuid/version'
 end
