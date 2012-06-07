a0e749078f896a60b34eb191165eb6d6ad573412
Chamnap <chamnapchhorn@gmail.com>
2012-06-07 22:49:04 +0700

    fix tests

--- lib/active_record_uuid.rb
+++ lib/active_record_uuid.rb
@@ -2,7 +2,7 @@ require 'uuidtools'
 require 'active_record'
 
 require 'active_record_uuid/uuid_base'
-require 'active_record_uuid/railtie'
+require 'active_record_uuid/railtie' if defined?(Rails)
 
 module ActiveRecordUuid
   autoload :VERSION, 'active_record_uuid/version'
--- lib/active_record_uuid/uuid_base.rb
+++ lib/active_record_uuid/uuid_base.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__), "uuid_base_helper.rb")
 
-class ::ActiveRecord::UuidBase < ::ActiveRecord::Base
+class ActiveRecord::UuidBase < ::ActiveRecord::Base
   include UuidBaseHelper
 
   class << self
