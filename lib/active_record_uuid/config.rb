033670ff184afe565199b5c4511c55208ab67fa2
Chamnap <chamnapchhorn@gmail.com>
2012-06-17 18:03:24 +0700

    refactor spec_helper and config

--- lib/active_record_uuid/config.rb
+++ lib/active_record_uuid/config.rb
@@ -1,36 +1,19 @@
 module ActiveRecordUuid
   class Config
-    def column(value=nil)
-      @column = value if value
-      @column
-    end
-    
-    def primary_key(value=nil)
-      @primary_key = value if value 
-      @primary_key
-    end
-    
-    def association(value=nil)
-      @association = value if value
-      @association
-    end
-    
-    def generator(value=nil)
-      @generator = value if value
-      @generator
-    end
-    
-    def store_as(value=nil)
-      @store_as = value if value
-      @store_as
+    METHODS = [:column, :primary_key, :association, :generator, :store_as]
+    METHODS.each do |meth|
+      define_method(meth.to_sym) do |value=nil|
+        instance_variable_set("@#{meth}".to_sym, value) if value
+        instance_variable_get("@#{meth}")
+      end
     end
     
     def assign_default!
-      @column = :uuid          if @column.nil?
-      @primary_key = false     if @primary_key.nil?
-      @association = false     if @association.nil?
-      @generator = :timestamp  if @generator.nil?
-      @store_as = :string      if @store_as.nil?
+      @column      = :uuid       if @column.nil?
+      @primary_key = false       if @primary_key.nil?
+      @association = false       if @association.nil?
+      @generator   = :timestamp  if @generator.nil?
+      @store_as    = :string     if @store_as.nil?
       
       # raise exception when there is not in the list
       default_generators = [:timestamp, :random]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,15 +6,18 @@ db_config = {
   :user => "root",
   :password => "Q1p2m3g4"
 }
-ActiveRecord::Base.establish_connection(db_config)
-ActiveRecord::Base.connection.drop_database(db_config[:database])
+
+ActiveRecord::Base.establish_connection(db_config) rescue nil
+ActiveRecord::Base.connection.drop_database(db_config[:database]) rescue nil
 ActiveRecord::Base.establish_connection(db_config.merge(:database => nil))
 ActiveRecord::Base.connection.create_database(db_config[:database], { :charset => 'utf8', :collation => 'utf8_unicode_ci' })
 ActiveRecord::Base.establish_connection(db_config)
 
+# load extension
 require 'active_record_uuid/extensions/quoting_extension'
 ::ActiveRecord::Base.connection.class.send :include, ActiveRecordUuid::QuotingExtension
 
+# load support
 load File.dirname(__FILE__) + '/support/schema.rb'
 load File.dirname(__FILE__) + '/support/models.rb'
 
